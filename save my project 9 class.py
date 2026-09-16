0eb711358f96d4662a40cead83f1be1a5c325d6c
tryasdev <tryasunov.maxim@mail.ru>
2026-09-16 18:01:39 +0300

    Add files via upload
    new beta 1.1

---
+++ save my project 9 class.py
@@ -0,0 +1,45 @@
+print('Welcome to NetworkShop "MotoTyt"\nEnter what you want to do?')
+parts = ['oil', 'carburator']
+price = [450, 7000]
+Quantity = [7, 2]
+number = 0
+while True:
+    human = input()
+    human2 = input()
+    if human == 'secret code':
+        print('welcome back boss')
+        boss = input()
+        if boss == 'new position':
+            print('who?')
+            new_parts = input()
+            parts.append(new_parts)
+            print('New parts:', parts)
+    elif human == 'buy':
+        print('Parts:', parts)
+        print('Price:', price)
+        print('Quantity:', Quantity)
+        human2 = input()
+        for part in parts:
+            if human2 in parts:
+                print('How much do you want to buy?')
+                quantity = int(input())
+                Quantity[0] = Quantity[0] - quantity
+                if Quantity[0] <= 0:
+                    print('There is not enough goods in the warehouse!')
+                else:
+                    print('What are you buying:', human2, '\nQuantity:', quantity, '\nThank You!''\nPrice:', price[0],
+                          '\nIn stock:', Quantity[0])
+        for part in parts:
+            if human2 in parts:
+                print('How much do you want to buy?')
+                quantity = int(input())
+                Quantity[1] = Quantity[1] - quantity
+                if Quantity[1] <= 0:
+                  print('There is not enough goods in the warehouse!')
+            else:
+                print('What are you buying:', human2, '\nQuantity:', quantity, '\nThank You!''\nPrice:', price[1],
+                      '\nIn stock:', Quantity[1])
+        else:
+            print('Sorry, we dont have such details')
+    else:
+        print('The command is not recognised')
